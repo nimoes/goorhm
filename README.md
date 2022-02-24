857f09a45539649b6160b32622f173b9b759ba77
MMattLeeE <mmattleee@gmail.com>
2022-02-24 18:36:59 -0500

    arn call

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # goorhm
 cloud things
 
-this is separate branch for mmattleee changes
+# setup:
+1. install aws cli https://aws.amazon.com/cli/
+2. run command
+    ```
+    >aws configure
+    ```
+3. follow prompts and enter in credentials
--- main.py
+++ main.py
@@ -7,10 +7,19 @@ import boto3
 import botocore
 import os
 import sys
+'''
+ec2 = boto3.client('ec2',region_name='us-east-1')
+response = ec2.describe_instances()
+print(response)
+'''
+
+
 
 client = boto3.client('sts', region_name='us-east-1')
 
 try:
-    account_info = client.get_caller_identity().get('Arn')
+    account_info = client.get_caller_identity()['Arn']
+    print(account_info)
 except botocore.exceptions.ClientError as error:
     sys.exit("couldn't retrieve account info")
+
