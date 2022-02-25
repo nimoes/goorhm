48b356ab15f25f4d6e81fa8936a9696327e328c7
MMattLeeE <mmattleee@gmail.com>
2022-02-24 19:07:52 -0500

    call all snapshots include public

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ response = ec2.describe_instances()
 print(response)
 '''
 
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+
+snapshots = ec2.snapshots.all()
+
+for s in snapshots:
+    print(f'Snapshot {s.id} for volume {s.volume_id}')
 
 
+"""
 client = boto3.client('sts', region_name='us-east-1')
 
 try:
@@ -23,3 +30,4 @@ try:
 except botocore.exceptions.ClientError as error:
     sys.exit("couldn't retrieve account info")
 
+"""
