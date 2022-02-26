5bd8796561caa284dad5dc7b16b2f240ada3aba0
MMattLeeE <mmattleee@gmail.com>
2022-02-26 12:53:09 -0500

    remove comments

--- main.py
+++ main.py
@@ -7,27 +7,16 @@ import boto3
 import botocore
 import os
 import sys
-'''
-ec2 = boto3.client('ec2',region_name='us-east-1')
-response = ec2.describe_instances()
-print(response)
-'''
+
 
 ec2 = boto3.resource('ec2', region_name='us-east-1')
 
 snapshots = ec2.snapshots.all()
 
+count = 0
 for s in snapshots:
+    count += 1
     print(f'Snapshot {s.id} for volume {s.volume_id}')
 
+print(count)
 
-"""
-client = boto3.client('sts', region_name='us-east-1')
-
-try:
-    account_info = client.get_caller_identity()['Arn']
-    print(account_info)
-except botocore.exceptions.ClientError as error:
-    sys.exit("couldn't retrieve account info")
-
-"""
