f815e3d5257e0feb8fb6a2255872718d79979fd6
nimoes <mhseo2180@gmail.com>
2022-02-26 17:23:51 -0800

    ebs enum, logging, saving to output option

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ inspired by dc19 on public ebs, this program will check if your assets are expos
 ## Setup
 ### Windows (PS)
 ```
-> mkdir goorhm
 > cd goorhm
 > python3 -m venv venv
 > .\venv\Scripts\activate
@@ -14,7 +13,6 @@ inspired by dc19 on public ebs, this program will check if your assets are expos
 
 ### Linux (Ubuntu) / MacOS
 ```
-> mkdir goorhm
 > cd goorhm
 > python3 -m venv venv
 > source ./venv/bin/activate
@@ -31,6 +29,19 @@ Based on specified region, the program will return a list of public EBS in json
 optional arguments:
   -h, --help            show this help message and exit
   -r REGION, --region REGION
-                        specify region ("us-west-1"
+                        specify region ("us-west-1")
   --preview             prints first 10 entries
+  -o OUTPUT, --output OUTPUT
+                        name of the output json file
+```
+
+```
+> python main.py -r "us-east-1" -o myoutput --preview
+
+2022-02-26 17:22:38,468 - EBS Snapshots Enumerator - INFO - Your current profile:       arn:aws:iam::621121731539:user/dev-boto3
+2022-02-26 17:22:38,470 - EBS Snapshots Enumerator - INFO - Checking for your vulnerable EBS snapshots...
+2022-02-26 17:22:38,569 - EBS Snapshots Enumerator - INFO - Retrieved EBS
+2022-02-26 17:22:38,815 - EBS Snapshots Enumerator - INFO - Printing a preview of output
+2022-02-26 17:22:38,816 - EBS Snapshots Enumerator - INFO - [('snap-081170e1eb6038193', {'tags': [{'Key': 'description', 'Value': 'dummyebs'}], 'aws_account_id': '621121731539', 'volume_id': 'vol-0accb1e4743bee2bf', 'backup_time': datetime.datetime(2022, 2, 26, 20, 7, 42, 673000, tzinfo=tzutc())})]
+2022-02-26 17:22:38,818 - EBS Snapshots Enumerator - INFO - Your output file has been generated
 ```
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import botocore
 import argparse
 import sys
 import logging
-import re
+import json
 
 
 def login_check(region) -> str:
@@ -27,19 +27,21 @@ def login_check(region) -> str:
     return account_id
 
 
-def ebsList(aws_account_id, reg):
+def ebsList(aws_account_id, reg, preview=False) -> dict:
     """
-    enumerate ebs in region
+    enumerate ebs based on specified region
     """
 
     ec2 = boto3.resource('ec2', region_name=reg)
 
     # list publicly available unencrypted snapshots
-    snapshots = ec2.snapshots.limit(count=1).filter(
-        MaxResults=10,
+    # if preview flag was used, print up to 10 entries in the console
+    count = 10 if preview == True else 0
+
+    snapshots = ec2.snapshots.all().filter(
         Filters = [
             {
-                'Name': 'owner_id',
+                'Name': 'owner-id',
                 'Values': [aws_account_id],
             },
             {
@@ -48,14 +50,29 @@ def ebsList(aws_account_id, reg):
             },
         ],
     )
+    
+    # if snapshot exists, print message
+    if snapshots:
+        logger.info("Retrieved EBS")
+    else:
+        logger.info("No vulnerable EBS found under this region")
+    
+    d= {}
 
-    # for ss in snapshots:
-    #     print(dir(ss))
-    #     print("owner_id" + str(ss.owner_id))
-    #     print("\nowner_alias" + str(ss.owner_alias))
-    #     print("\nstate" + str(ss.state))
-    #     print("\nstart_time" + str(ss.start_time))
-    return
+    # prep output
+    for ss in snapshots:
+        d[ss.id] = {}
+        d[ss.id]['tags'] = ss.tags
+        d[ss.id]['aws_account_id'] = ss.owner_id
+        d[ss.id]['volume_id'] = ss.volume_id
+        d[ss.id]['backup_time'] = ss.start_time
+    
+    # if preview flag was used, print a preview with up to 10 entries
+    if preview:
+        logger.info("Printing a preview of output")
+        logger.info(list(d.items())[:count])
+
+    return d
 
 
 if __name__ == '__main__':
@@ -67,35 +84,42 @@ if __name__ == '__main__':
          help="specify region (\"us-west-1\")")
     parser.add_argument("--preview", required=False, action='store_true', \
         help="prints first 10 entries")
+    parser.add_argument("-o", "--output", required=False, help="name of the \
+        output json file")
 
     # parse args
     args = parser.parse_args()
     REGION_NAME = args.region
     PREVIEW = True if args.preview else False
+    OUTPUT = args.output
 
     # logger
     logger = logging.getLogger('EBS Snapshots Enumerator')
     logger.setLevel(logging.DEBUG)
-
+    # console handler
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-
     # create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    # add formatter to ch
+    # add formatter to console handler
     ch.setFormatter(formatter)
-
-    # add ch to logger
+    # add console event handler to logger
     logger.addHandler(ch)
+
 
     # check if authentication is successful, returns account id
     aws_account_id = login_check(REGION_NAME)
 
-    logger.info("Checking for vulnerable EBS snapshots...")
+    logger.info("Checking for your vulnerable EBS snapshots...")
 
     if aws_account_id:
-        ebsList(aws_account_id, REGION_NAME)
+        ebs_output = ebsList(aws_account_id, REGION_NAME, PREVIEW)
     else:
         sys.exit(1)
     
+    # save output, if specified
+    if OUTPUT:
+        ff = str(OUTPUT)+'.json'
+        with open(ff, 'w') as f:
+            json.dump(ebs_output, f, default=str)
+        logger.info("Your output file has been generated")
